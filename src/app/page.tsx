9e941c3bf2036a7fe803640fbc9eda5c86b0a211
VanuPr <vanuoragnic@gmail.com>
2025-08-05 12:29:49 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AuthenticatedLayout from "./dashboard/layout";
 import DashboardPage from "./dashboard/page";
 
 export default function Home() {
   return (
-    <AuthenticatedLayout>
       <DashboardPage />
-    </AuthenticatedLayout>
   );
 }
